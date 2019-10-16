18253232d22e5c1c70d0b5a7ccd05744e7c0f0d8
kmurata798 <kmurata798@gmail.com>
2019-10-16 13:27:25 -0700

    / route leads to posts.find()

--- app.py
+++ app.py
@@ -14,15 +14,15 @@ posts = db.posts
 #     '''returns homepage'''
 #     return render_template('home.html', msg = 'I am good')
 
-posts = [
-    { 'title': 'Math Question', 'description': 'NEED HELP WITH PROBLEM 1' },
-    { 'title': 'Song requests', 'description': 'Send me any songs to listen to!' }
-]
+# posts = [
+#     { 'title': 'Math Question', 'description': 'NEED HELP WITH PROBLEM 1' },
+#     { 'title': 'Song requests', 'description': 'Send me any songs to listen to!' }
+# ]
 
 @app.route('/')
 def posts_index():
     """Show all posts."""
-    return render_template('posts_index.html', posts=posts)
+    return render_template('posts_index.html', posts=posts.find())
 
 if __name__ == '__main__':
     app.run(debug=True)
