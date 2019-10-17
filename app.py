9d995b850a222ddb3dcb6e05662002ef13559c18
kmurata798 <kmurata798@gmail.com>
2019-10-17 10:38:57 -0700

    GREAT amount of changes made

--- app.py
+++ app.py
@@ -25,25 +25,42 @@ def posts_submit():
     post = {
         'title': request.form.get('title'),
         'description': request.form.get('description'),
-        'videos': request.form.get('videos').split(),
+        'videos': request.form.get('videos'),
         'topic': request.form.get('topic')
     }
     # posts.insert_one(post)
     post_id = posts.insert_one(post).inserted_id
+    print(post_id)
     return redirect(url_for('posts_show', post_id=post_id))
 
-@app.route('/posts/<post_id>')
+@app.route('/posts/<post_id>', methods=['GET'])
 def posts_show(post_id):
+    
     '''show specific post'''
-    post = posts.find_one({'_id': ObjectId(post_id)})
-    # return f'my ID is {post_id}'
-    return render_template('posts_show.html', post=post)
+    if request.method == 'GET':
+        
+        post = posts.find_one({'_id': ObjectId(post_id)})
+        print(post)
+        # return f'my ID is {post_id}'
+        return render_template('posts_show.html', post=post)
 
-@app.route('/posts/<post_id>/edit')
+@app.route('/posts/<post_id>/edit', methods=['GET', 'POST'])
 def posts_edit(post_id):
     '''shows the editing form for a post'''
-    post = posts.find_one({'_id': ObjectId(post_id)})
-    return render_template('posts_edit.html', post=post, title='Edit Post')
+    if request.method == 'GET':
+        post = posts.find_one({'_id': ObjectId(post_id)})
+        return render_template('posts_edit.html', post=post)
+    if request.method == 'POST':
+        post = {
+        'title': request.form.get('title'),
+        'description': request.form.get('description'),
+        'videos': request.form.get('videos'),
+        'topic': request.form.get('topic')
+        }
+        posts.update_one({'_id': ObjectId(post_id)}, {'$set': post})
+        return redirect(url_for('posts_index'))
+     
+
 
 if __name__ == '__main__':
     app.run(debug=True)
